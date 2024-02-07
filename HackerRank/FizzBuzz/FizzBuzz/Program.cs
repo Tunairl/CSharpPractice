a05b49194153ec6dd0e091a35c6f233325dcbb8e
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-02-07 13:54:04 +0300

    Add files via upload

---
+++ HackerRank/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        fizzBuzz(13);
+    }
+
+    public static void fizzBuzz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 != 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0 && i % 3 != 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
