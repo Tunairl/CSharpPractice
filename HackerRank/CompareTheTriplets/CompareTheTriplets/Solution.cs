57084223d3556afabc4827ec92573040abc17345
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-02-11 17:44:04 +0300

    Add files via upload

---
+++ HackerRank/CompareTheTriplets/CompareTheTriplets/Solution.cs
@@ -0,0 +1,25 @@
+﻿
+class Solution
+{
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+      
+        var result = new List<int> { 0, 0 };
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (a[i] > b[i])
+            {
+                result[0]++;
+            }
+            else if (a[i] < b[i])
+            {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+}
+
+
+
